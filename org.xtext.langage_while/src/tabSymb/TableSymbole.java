0e046148b762b4f321bcb582974bf0d99543bda3
alimohamed92 <alimohamedaffizou@gmail.com>
2015-11-23 11:02:53 +0100

    tabSymbole

---
+++ org.xtext.langage_while/src/tabSymb/TableSymbole.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package tabSymb;
+
+/**
+ * @author Ali Mohamed
+ *
+ */
+public class TableSymbole {
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
