711f22eaa862ca9b10ac0132b24a5d349afc3fe5
steve <steve@148.60.14.142>
2015-12-14 15:22:35 +0100

    Codage de l'arbre binaire représentant les notions nil, l'arbre while.

--- org.xtext.langage_while/src/code3a/BinTreeWh.java
+++ org.xtext.langage_while/src/code3a/BinTreeWh.java
@@ -1,9 +1,40 @@
 package code3a;
 
-import java.util.*;
-
 public class BinTreeWh {
-	TreeSet arbre;
-	
 
+	public static final BinTreeWh NIL = new BinTreeWh("nil");
+	
+	private String noeud;
+	private BinTreeWh sag;
+	private BinTreeWh sad;
+	
+	private BinTreeWh(String name){
+		this.noeud = name;
+		sag = null;
+		sad = null;
+	}
+	
+	private BinTreeWh(BinTreeWh a, BinTreeWh b) {
+		this.noeud = a.getName() + "_" + b.getName();
+		this.sag = a;
+		this.sad = b;
+	}
+	
+	public String getName(){
+		return noeud;
+	}
+	
+	public boolean isNil(){
+		return (sag == null && sad == null);
+	}
+	
+	/**
+	 * Construit un arbre à partir de deux arbres
+	 * @param A Arbre while
+	 * @param B Arbre while
+	 * @return Arbre contenant les deux passés en paramètres
+	 */
+	public static BinTreeWh cons(BinTreeWh A, BinTreeWh B){
+		return new BinTreeWh(A, B);
+	}
 }
