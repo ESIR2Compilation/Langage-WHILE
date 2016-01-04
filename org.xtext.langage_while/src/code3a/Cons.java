7e9dbb370628610d2afcb09d7354f8e992c5495a
Sven <othmansanhaji@gmail.com>
2016-01-04 17:39:14 +0100

    Modif Cons

--- org.xtext.langage_while/src/code3a/Cons.java
+++ org.xtext.langage_while/src/code3a/Cons.java
@@ -3,15 +3,25 @@ package code3a;
 import libWhile.BinTree;
 
 public class Cons {
+	private String racine;
+	private BinTree<Chevron> arbrein1;
+	private BinTree<Chevron> arbrein2;
 
-	private BinTree<Chevron> arbre;
 	
-	private Cons(BinTree<Chevron> a,BinTree<Chevron> b){
-		
-		this.arbre=new BinTree<Chevron>();
-		arbre.addHd(a);
-		arbre.addTl(b);
+	private Cons(String r,BinTree<Chevron> a,BinTree<Chevron> b){
+		this.racine=r;
+		this.arbrein1=a;
+		this.arbrein2=b;
+
 	}
+	public BinTree<Chevron> getArbrein1(){
+		return arbrein1;
+	}
+	
+	public BinTree<Chevron> getArbrein2(){
+		return arbrein2;
+	}
+
 
 	
 }
